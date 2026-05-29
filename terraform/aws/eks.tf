1b8fa724b165a6a1e97cbf665a59c155a1c3b26a
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-05-29 18:14:39 +0300

    enabled jenkins to have admin access inside the eks

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -3,11 +3,12 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name               = "${var.environment}-clinic-cluster"
-  kubernetes_version = "1.33"
+  name                                     = "${var.environment}-clinic-cluster"
+  kubernetes_version                       = "1.33"
   enable_cluster_creator_admin_permissions = var.enable_eks_bootstrap_cluster_creator_admin_permissions
 
-  endpoint_public_access = var.enable_eks_endpoint_public_access
+  endpoint_public_access  = var.enable_eks_endpoint_public_access
+  endpoint_private_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
@@ -19,6 +20,22 @@ module "eks" {
       max_size       = var.node_max_size
       desired_size   = var.node_desired_size
 
+    }
+  }
+
+  access_entries = {
+    jenkins = {
+      kubernetes_groups = ["system:masters"]
+      principal_arn     = aws_iam_role.jenkins.arn
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
     }
   }
 
