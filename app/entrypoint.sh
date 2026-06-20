73aefd2778aa71d41088749c82a6255236431eea
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-06-20 15:37:36 +0300

    added dockerfile for nginx, with static copied from web image

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,5 +5,4 @@ echo "Waiting for postgres..."
 python manage.py migrate
 python manage.py create_groups
 python manage.py seed_data
-python manage.py collectstatic --noinput
 exec gunicorn clinic_appointment.wsgi:application --bind 0.0.0.0:8000 --workers 3
