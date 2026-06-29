643eb3a814ac534e608b8ebd9c6b292699028cf4
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-06-29 19:48:24 +0300

    added prometheus app to django project

--- app/clinic_appointment/settings.py
+++ app/clinic_appointment/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost").split(","
 # Application definition
 
 INSTALLED_APPS = [
+    'django_prometheus',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
 
 ROOT_URLCONF = 'clinic_appointment.urls'
--- app/clinic_appointment/urls.py
+++ app/clinic_appointment/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
+    path('', include('django_prometheus.urls')),
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('appointments/', include('appointments.urls')),
